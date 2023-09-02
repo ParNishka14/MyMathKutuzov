cde1cbcccc8da7aa56755d5b0cfb787d2362cfa3
Andrey <derty7737@gmail.com>
2023-09-02 16:30:27 +0300

    Add Methods: sin() cos() tg() areaCircle() lengthCircle() toRadians()

--- src/main/java/org/example/MyMath.java
+++ src/main/java/org/example/MyMath.java
@@ -4,14 +4,41 @@ public class MyMath {
      public static final double PI = 3.141592653589793;
      public static final double E = 2.718281828459045;
 
-     double tg(){
+     public static double tg(double degrees){
 
-         return 0;
+          return sin(degrees)/cos(degrees);
      }
-     double cos(){
-          return 0;
+     public static double cos(double degrees){
+
+          double sin = sin(degrees);
+          return  Math.sqrt(1- sin * sin);  //использование библиотеки Math, простите((
      }
-     double sin(){
-          return 0;
+     public static double sin(double degrees){
+
+          double radians = toRadians(degrees);
+          double sin = 0;
+          double progress = radians;
+
+          // разложение в ряд Тейлора для синуса, точность +-0.0001
+          for (int i = 1; i <= 10; i++){
+               sin += progress;
+               progress *= -(radians * radians)/((2 * i)*(2 * i + 1));
+          }
+
+          return sin;
+     }
+
+     public static double areaCircle(double radius){
+
+          return PI * (radius * radius);
+     }
+     public static double lengthCircle(double radius){
+
+          return 2 * PI * radius;
+     }
+
+     public static double toRadians(double degrees){
+          
+          return degrees * (PI/180);
      }
 }
